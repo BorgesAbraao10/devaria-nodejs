1a3dc2ef08bb30696a4efb636769868ce77acbc4
Abraão Borges <abraao10clear@gmail.com>
2023-11-09 19:19:54 -0300

    Update app.js

--- variaveis e input/app.js
+++ variaveis e input/app.js
@@ -1,2 +1,11 @@
+const readLine = require('readline').createInterface({
+  input: process.stdin,
+  outinput: process.stdout
+})
+
 const minhaPrimeiraConstanteString = "My first constant";
 console.log(minhaPrimeiraConstanteString);
+
+readLine
+
+
