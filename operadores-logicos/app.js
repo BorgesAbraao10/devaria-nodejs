f85a0a617f39deb1a7bfa5f83f93ab0be71d7bbc
Abraão Borges <abraao10clear@gmail.com>
2023-11-13 12:32:30 -0300

    Update app.js

--- operadores-logicos/app.js
+++ operadores-logicos/app.js
@@ -9,3 +9,30 @@ console.log(
 console.log(
   "Além das suas verificações, precisamos consfirmar se está na lista de reserva"
 );
+
+readline.question("Qual ano você nasceu?", (ano) => {
+  if (ano > 2004) {
+    console.log("Você não tem 18 anos");
+  } else {
+    readline.question("Você tem habilitação? (Sim/Não)", (temHabilitacao) => {
+      if (!(temHabilitacao.toUpperCase() === "SIM")) {
+        console.log("Você não tem habilitação para entrar no Kart");
+      } else {
+        readline.question("Qual é o seu nome?", (nome) => {
+          switch (nome) {
+            case "Douglas":
+              console.log("Bem vindo ao Kart Douglas");
+              break;
+            case "Rafael":
+              console.log("Bem vindo ao Kart Rafael ");
+              break;
+            default:
+              console.log(
+                "Seu nome não foi identificado na lista de presença:"
+              );
+          }
+        });
+      }
+    });
+  }
+});
