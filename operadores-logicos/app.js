9c503d9970e58fd016ef5059f0cb9d989d4fce2c
Abraão Borges <abraao10clear@gmail.com>
2023-11-13 11:57:07 -0300

    Create app.js

---
+++ operadores-logicos/app.js
@@ -0,0 +1,11 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log(
+  "Esse programa vai checar se você é maior de 18 anos e tem habilitação para entrar no Kart"
+);
+console.log(
+  "Além das suas verificações, precisamos consfirmar se está na lista de reserva"
+);
